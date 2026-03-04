0d69341cd0ade68ec92c3085aea66e4cd6f0e0e2
josealonsogt <josealonsogt@icloud.es>
2026-03-04 14:07:42 +0100

    fix: Use environment variables for Firebase config

--- services/firebaseConfig.js
+++ services/firebaseConfig.js
@@ -2,17 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAYlifIi8EY3s8TSiI5mxnLKLeSfhc7Vb4",
-  authDomain: "torneof1.firebaseapp.com",
-  projectId: "torneof1",
-  storageBucket: "torneof1.firebasestorage.app",
-  messagingSenderId: "64983276789",
-  appId: "1:64983276789:web:8c754396956a9933d039bc",
-  measurementId: "G-2CR21TP8BJ",
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
-// Inicializamos la app de Firebase
 const app = initializeApp(firebaseConfig);
-
-// Inicializamos la base de datos (Firestore) y la exportamos para poder usarla en el resto de la app
 export const db = getFirestore(app);
